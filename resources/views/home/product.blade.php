2962544adf6c8bf6acfb2bd96b74e1a228488357
ersavlrian <esra.valerian@gmail.com>
2024-12-17 21:09:15 +0700

    Update

--- resources/views/home/product.blade.php
+++ resources/views/home/product.blade.php
@@ -1,216 +1,216 @@
 <section class="shop_section layout_padding">
-    <div class="container">
-      <div class="heading_container heading_center">
-        <h2>
-          Latest Products
-        </h2>
-      </div>
-      <div class="row">
-        <div class="col-sm-6 col-md-4 col-lg-3">
-          <div class="box">
-            <a href="">
-              <div class="img-box">
-                <img src="images/p1.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h6>
-                  Ring
-                </h6>
-                <h6>
-                  Price
-                  <span>
-                    $200
-                  </span>
-                </h6>
-              </div>
-              <div class="new">
+  <div class="container">
+    <div class="heading_container heading_center">
+      <h2>
+        Latest Products
+      </h2>
+    </div>
+    <div class="row">
+      <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="box">
+          <a href="">
+            <div class="img-box">
+              <img src="images/bakpao.jpg" alt="">
+            </div>
+            <div class="detail-box">
+              <h6>
+                Bakpao
+              </h6>
+              <h6>
+                Harga
                 <span>
-                  New
+                  Rp.5000
                 </span>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4 col-lg-3">
-          <div class="box">
-            <a href="">
-              <div class="img-box">
-                <img src="images/p2.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h6>
-                  Watch
-                </h6>
-                <h6>
-                  Price
-                  <span>
-                    $300
-                  </span>
-                </h6>
-              </div>
-              <div class="new">
-                <span>
-                  New
-                </span>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4 col-lg-3">
-          <div class="box">
-            <a href="">
-              <div class="img-box">
-                <img src="images/p3.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h6>
-                  Teddy Bear
-                </h6>
-                <h6>
-                  Price
-                  <span>
-                    $110
-                  </span>
-                </h6>
-              </div>
-              <div class="new">
-                <span>
-                  New
-                </span>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4 col-lg-3">
-          <div class="box">
-            <a href="">
-              <div class="img-box">
-                <img src="images/p4.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h6>
-                  Flower Bouquet
-                </h6>
-                <h6>
-                  Price
-                  <span>
-                    $45
-                  </span>
-                </h6>
-              </div>
-              <div class="new">
-                <span>
-                  New
-                </span>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4 col-lg-3">
-          <div class="box">
-            <a href="">
-              <div class="img-box">
-                <img src="images/p5.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h6>
-                  Teddy Bear
-                </h6>
-                <h6>
-                  Price
-                  <span>
-                    $95
-                  </span>
-                </h6>
-              </div>
-              <div class="new">
-                <span>
-                  New
-                </span>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4 col-lg-3">
-          <div class="box">
-            <a href="">
-              <div class="img-box">
-                <img src="images/p6.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h6>
-                  Flower Bouquet
-                </h6>
-                <h6>
-                  Price
-                  <span>
-                    $70
-                  </span>
-                </h6>
-              </div>
-              <div class="new">
-                <span>
-                  New
-                </span>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4 col-lg-3">
-          <div class="box">
-            <a href="">
-              <div class="img-box">
-                <img src="images/p7.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h6>
-                  Watch
-                </h6>
-                <h6>
-                  Price
-                  <span>
-                    $400
-                  </span>
-                </h6>
-              </div>
-              <div class="new">
-                <span>
-                  New
-                </span>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div class="col-sm-6 col-md-4 col-lg-3">
-          <div class="box">
-            <a href="">
-              <div class="img-box">
-                <img src="images/p8.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h6>
-                  Ring
-                </h6>
-                <h6>
-                  Price
-                  <span>
-                    $450
-                  </span>
-                </h6>
-              </div>
-              <div class="new">
-                <span>
-                  New
-                </span>
-              </div>
-            </a>
-          </div>
+              </h6>
+            </div>
+            <div class="new">
+              <span>
+                New
+              </span>
+            </div>
+          </a>
         </div>
       </div>
-      <div class="btn-box">
-        <a href="">
-          View All Products
-        </a>
+      <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="box">
+          <a href="">
+            <div class="img-box">
+              <img src="images/bolu gulung.jpg" alt="">
+            </div>
+            <div class="detail-box">
+              <h6>
+                Bolu Gulung
+              </h6>
+              <h6>
+                Harga
+                <span>
+                  Rp.15000
+                </span>
+              </h6>
+            </div>
+            <div class="new">
+              <span>
+                New
+              </span>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="box">
+          <a href="">
+            <div class="img-box">
+              <img src="images/bolu kukus mekar.jpg" alt="">
+            </div>
+            <div class="detail-box">
+              <h6>
+                Bolu Kukus Mekar
+              </h6>
+              <h6>
+                Harga
+                <span>
+                  Rp.4000
+                </span>
+              </h6>
+            </div>
+            <div class="new">
+              <span>
+                New
+              </span>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="box">
+          <a href="">
+            <div class="img-box">
+              <img src="images/bolu pandan.jpg" alt="">
+            </div>
+            <div class="detail-box">
+              <h6>
+                Bolu Pandan
+              </h6>
+              <h6>
+                Harga
+                <span>
+                  Rp.15000
+                </span>
+              </h6>
+            </div>
+            <div class="new">
+              <span>
+                New
+              </span>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="box">
+          <a href="">
+            <div class="img-box">
+              <img src="images/bolu pisang.jpg" alt="">
+            </div>
+            <div class="detail-box">
+              <h6>
+                Bolu Pisang
+              </h6>
+              <h6>
+                Harga
+                <span>
+                  Rp.15000
+                </span>
+              </h6>
+            </div>
+            <div class="new">
+              <span>
+                New
+              </span>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="box">
+          <a href="">
+            <div class="img-box">
+              <img src="images/cookies coklat.jpg" alt="">
+            </div>
+            <div class="detail-box">
+              <h6>
+                Cookies Coklat
+              </h6>
+              <h6>
+                Harga
+                <span>
+                  Rp.10000
+                </span>
+              </h6>
+            </div>
+            <div class="new">
+              <span>
+                New
+              </span>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="box">
+          <a href="">
+            <div class="img-box">
+              <img src="images/Croissant.jpg" alt="">
+            </div>
+            <div class="detail-box">
+              <h6>
+                Croissant
+              </h6>
+              <h6>
+                Harga
+                <span>
+                  Rp.7000
+                </span>
+              </h6>
+            </div>
+            <div class="new">
+              <span>
+                New
+              </span>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="box">
+          <a href="">
+            <div class="img-box">
+              <img src="images/nastar.jpg" alt="">
+            </div>
+            <div class="detail-box">
+              <h6>
+                Nastar
+              </h6>
+              <h6>
+                Harga
+                <span>
+                  Rp.30000
+                </span>
+              </h6>
+            </div>
+            <div class="new">
+              <span>
+                New
+              </span>
+            </div>
+          </a>
+        </div>
       </div>
     </div>
+    <div class="btn-box">
+      <a href="">
+        View All Products
+      </a>
+    </div>
+  </div>
   </section>
