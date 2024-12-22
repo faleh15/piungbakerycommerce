bd78c4894096ad2dcb586d0e8a6bcfb322d51154
Toniyahyama <toniyahya451@gmail.com>
2024-12-23 05:34:11 +0700

    Update

--- resources/views/admin/order.blade.php
+++ resources/views/admin/order.blade.php
@@ -52,6 +52,8 @@
                     <th>Price</th>
                     <th>Image</th>
                     <th>status</th>
+                    <th>Change Status</th>
+                    <th>Print PDF</th>
                 </tr>
                 @foreach ($data as $data)
                 <tr>
@@ -63,7 +65,35 @@
                     <td>
                         <img width="120px" src="products/{{$data->product->image}}">
                     </td>
-                    <td>{{$data->status}}</td>
+                    <td>
+
+                    @if ($data->status == 'in progress')
+
+                    <span style="color:red">{{$data->status}}</span>
+
+                    @elseif($data->status == 'On the way')
+
+                    <span style="color:skyblue">{{$data->status}}</span>
+
+                    @else 
+                    <span style="color:yellow">{{$data->status}}</span>
+
+        
+                    @endif
+
+                    </td>
+
+                    <td>
+                      <a class="btn btn-primary" href="{{url('on_the_way',$data->id)}}">On the way</a>
+
+                      <a class="btn btn-success" href="{{url('delivered',$data->id)}}">Delivered</a>
+                      
+                    </td>
+
+                    <td>
+                    <a class="btn btn-secondary" href="{{url('print_pdf',$data->id)}}">Print PDF<a/>
+                    </td>
+                    
                 </tr> 
                 @endforeach
             </table>
