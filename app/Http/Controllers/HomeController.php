638959451df93134bfbcfc31298fabbf4824fd4b
Toniyahyama <toniyahya451@gmail.com>
2024-12-23 05:08:49 +0700

    Update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,9 +17,14 @@ use Illuminate\Support\Facades\Auth;
 class HomeController extends Controller
 {
     public function index()
-    {
-        return view('admin.index');
-    }
+{
+    $user = User::where('usertype', 'user')->get()->count();
+    $product = Product::all()->count();
+    $order = Order::all()->count();
+    $delivered = Order::where('status', 'Delivered')->get()->count();
+    return view('admin.index', compact('user', 'product', 'order', 'delivered'));
+}
+
     
     public function home()
     {
@@ -154,7 +159,15 @@ class HomeController extends Controller
         return redirect()->back(); 
     }
     
-    
+    public function myorders()
+    {
+        $user = Auth::user()->id;
+
+        $count = Cart::where('user_id',$user)->get()->count();
+
+        $order = Order::where('user_id',$user)->get();
+        return view('home.order',compact('count','order'));
+    }
 
 
 }
